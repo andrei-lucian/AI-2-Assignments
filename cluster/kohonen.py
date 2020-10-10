5795d8c973fb30be6c8d5a3c0b60542ad24be39f
Andrei <lucianandrei12@gmail.com>
2020-10-10 18:15:13 +0200

    finished kohonen code

--- cluster/kohonen.py
+++ cluster/kohonen.py
@@ -1,3 +1,5 @@
+import random as r
+import math
 class Cluster:
     """This class represents the clusters, it contains the
     prototype (the mean of all it's members) and memberlists with the
@@ -23,27 +25,83 @@ class Kohonen:
         # The accuracy and hitrate are the performance metrics (i.e. the results)
         self.accuracy = 0
         self.hitrate = 0
+        # Initialise N x N clusters
+        for x in range(n):
+            for y in range(n):
+                self.clusters[x][y].prototype = self.traindata[r.randint(0,len(self.traindata)-1)]
+                print(self.clusters[x][y].prototype)
 
     def train(self):
         # Step 1: initialize map with random vectors (A good place to do this, is in the initialisation of the clusters)
         # Repeat 'epochs' times:
-        #     Step 2: Calculate the squareSize and the learningRate, these decrease lineary with the number of epochs.
+        #     Step 2: Calculate the squareSize and the learningRate, these decrease linearly with the number of epochs.
         #     Step 3: Every input vector is presented to the map (always in the same order)
         #     For each vector its Best Matching Unit is found, and :
         #         Step 4: All nodes within the neighbourhood of the BMU are changed, you don't have to use distance relative learning.
         # Since training kohonen maps can take quite a while, presenting the user with a progress bar would be nice
-        pass
+        r = 0
+        eta = 0 
+        for currEpoch in range(self.epochs):
+            eta = self.initial_learning_rate * (1 - currEpoch/self.epochs)
+            r = self.n/2 * (1 - currEpoch/self.epochs)
+
+            for i in range(self.n):
+                for j in range(self.n):
+                    self.clusters[i][j].current_members.clear()
+            
+            for i in range(len(self.traindata)-1):
+                minX = 0
+                minY = 0
+                distance = 0
+                minDistance = float('inf')
+                for j in range(self.n):
+                    for k in range(self.n):
+                        for m in range(self.dim):
+                            distance += math.pow(self.clusters[j][k].prototype[m] - self.traindata[i][m], 2)
+                        distance = math.sqrt(distance)
+                        if (distance <= minDistance):
+                            minX = j
+                            minY = k
+                            minDistance = distance
+
+                upperBoundX = min(self.n-1, minX + int(r))
+                for n in range(max(0, minX - int(r)), upperBoundX):
+                    upperBoundY = min(self.n - 1, minY + int(r))
+                    for o in range(max(0, minY - int(r)), upperBoundY):
+                        c = self.clusters[n][o]
+                        newPrototype = []
+                        for p in range(self.dim):
+                            newPrototype.append(float((1-eta) * c.prototype[p] + eta * self.traindata[i][p]))
+                        self.clusters[n][o].prototype = newPrototype
+                self.clusters[minX][minY].current_members.add(i)
 
     def test(self):
         # iterate along all clients
+        hits = 0
+        requests = 0
+        prefetch = 0
+
         # for each client find the cluster of which it is a member
-        # get the actual testData (the vector) of this client
-        # iterate along all dimensions
-        # and count prefetched htmls
-        # count number of hits
-        # count number of requests
+        for client in range(len(self.traindata)):
+            for i in range(self.n):
+                for j in range(self.n):
+                    if (client in self.clusters[i][j].current_members):
+                        owner = self.clusters[i][j]
+            # get the actual testData (the vector) of this client
+            test = self.testdata[client]
+            # iterate along all dimensions
+            for i in range(self.dim):
+                if(owner.prototype[i] > self.prefetch_threshold and 
+                    test[i] > self.prefetch_threshold):
+                    hits += 1         # count number of hits
+                if(test[i] > self.prefetch_threshold):
+                    requests += 1     # count number of requests
+                if(owner.prototype[i] > self.prefetch_threshold):
+                    prefetch += 1     # count prefetched htmls
+
         # set the global variables hitrate and accuracy to their appropriate value
-        pass
+        self.accuracy = hits/prefetch
+        self.hitrate = hits/requests
 
     def print_test(self):
         print("Prefetch threshold =", self.prefetch_threshold)
