f01127263eb14f353aa8a4d7b70d387107ef4dbb
Annika Möller <annika.moeller24@gmail.com>
2020-10-11 16:22:11 +0200

    split all of kohonenn into functions

--- cluster/kohonen.py
+++ cluster/kohonen.py
@@ -25,85 +25,124 @@ class Kohonen:
         ## The accuracy and hitrate are the performance metrics (i.e. the results)
         self.accuracy = 0
         self.hitrate = 0
-        ## Initialise N x N clusters
-        ## Step 1: initialize map with random vectors (A good place to do this, is in the initialisation of the clusters)
+        ## Initialise hits, requests, and prefetches
+        self.hits = 0
+        self.requests = 0
+        self.prefetch = 0
+        ## Initialise owner cluster object
+        self.owner = Cluster(dim)
+        ## Initialise inidices of closest clusters
+        self.min_d1 = 0
+        self.min_d2 = 0
+        ## Initialise upper and lower bounds for neighbourhood of clusters
+        self.upper_bound_d1 = 0
+        self.lower_bound_d1 = 0
+        self.upper_bound_d2 = 0
+        self.lower_bound_d2 = 0
+        ## Initialise radius and learning rate
+        self.radius = 0
+        self.learning_rate = 0
+        ## Initialise index of current training data vector
+        self.currIdx = 0 
+        ## Initialise map of N x N clusters with random input vectors
         for x in range(n):
             for y in range(n):
                 self.clusters[x][y].prototype = self.traindata[r.randint(0,len(self.traindata)-1)]
 
+    ## Clear all clusters' current members
+    def clearCurrentMembers(self):
+        for dim_1 in range(self.n):
+            for dim_2 in range(self.n):
+                self.clusters[dim_1][dim_2].current_members.clear()
+
+    ## Find an input vector's best mactching unit
+    def findBMU(self):
+        distance = 0
+        min_dist = float('inf')
+        for dim_1 in range(self.n):
+            for dim_2 in range(self.n):
+                for vect_dim in range(self.dim):
+                    ## Calculate Euclidian distance between input vector and each cluster
+                    distance += math.pow(self.clusters[dim_1][dim_2].prototype[vect_dim] 
+                    - self.traindata[self.currIdx][vect_dim], 2)
+                distance = math.sqrt(distance)
+                ## Store the indices of the best matching unit (closest cluster)
+                if (distance <= min_dist):
+                    self.min_d1 = dim_1
+                    self.min_d2 = dim_2
+                    min_dist = distance
+
+    ## Calculate the neighbourhood of a cluster
+    def calculateNeighbourhood(self):
+        self.upper_bound_d1 = min(self.n-1, self.min_d1 + int(self.radius)) 
+        self.lower_bound_d1 = max(0, self.min_d1 - int(self.radius))
+        self.upper_bound_d2 = min(self.n - 1, self.min_d2 + int(self.radius))
+        self.lower_bound_d2 = max(0, self.min_d2 - int(self.radius))
+
+    ## Update the prototype of a cluster
+    def updateClusterPrototypes(self):
+        for dim_1 in range(self.lower_bound_d1, self.upper_bound_d1):
+            for dim_2 in range(self.lower_bound_d2, self.upper_bound_d2):
+                cluster = self.clusters[dim_1][dim_2] 
+                new_prot = [] 
+                for vect_dim in range(self.dim): 
+                    new_prot.append(float((1-self.learning_rate) * cluster.prototype[vect_dim] 
+                    + self.learning_rate * self.traindata[self.currIdx][vect_dim]))
+                self.clusters[dim_1][dim_2].prototype = new_prot
+
+    ## Find the cluster that a client belongs to 
+    def findOwner(self, client):
+        for dim_1 in range(self.n):
+            for dim_2 in range(self.n):
+                if (client in self.clusters[dim_1][dim_2].current_members):
+                    self.owner = self.clusters[dim_1][dim_2]
+
+    ## Calculate the number of hits, requests, and prefetches
+    def calculatePerformance(self, client):
+        for vect_dim in range(self.dim):
+            if(self.owner.prototype[vect_dim] > self.prefetch_threshold and 
+                self.testdata[client][vect_dim] > self.prefetch_threshold):
+                self.hits += 1         ## count number of hits
+
+            if(self.testdata[client][vect_dim] > self.prefetch_threshold):
+                self.requests += 1     ## count number of requests
+
+            if(self.owner.prototype[vect_dim] > self.prefetch_threshold):
+                self.prefetch += 1     ## count prefetched htmls
+
     def train(self):
         ## Repeat 'epochs' times:
-        ## Step 2: Calculate the squareSize and the learningRate, these decrease linearly with the number of epochs.
-        for curr_epoch in range(self.epochs):
-            learning_rate = self.initial_learning_rate * (1 - curr_epoch/self.epochs)
-            radius = self.n/2 * (1 - curr_epoch/self.epochs)
+        for curr_epoch in range(self.epochs): 
 
-            for dim_1 in range(self.n):
-                for dim_2 in range(self.n):
-                    self.clusters[dim_1][dim_2].current_members.clear()
+            ## Calculate radius and learning_rate (both decrease linearly with epochs)
+            self.learning_rate = self.initial_learning_rate * (1 - curr_epoch/self.epochs)
+            self.radius = self.n/2 * (1 - curr_epoch/self.epochs)
 
-            ## Step 3: Every input vector is presented to the map (always in the same order)
-            ## For each vector its Best Matching Unit is found
-            for idx in range(len(self.traindata)-1):
-                distance = 0
-                min_dist = float('inf')
-                for dim_1 in range(self.n):
-                    for dim_2 in range(self.n):
-                        for vect_dim in range(self.dim):
-                            distance += math.pow(self.clusters[dim_1][dim_2].prototype[vect_dim] - self.traindata[idx][vect_dim], 2)
-                        distance = math.sqrt(distance)
-                        ## Store the indices of the best matching unig
-                        if (distance <= min_dist):
-                            min_d1 = dim_1
-                            min_d2 = dim_2
-                            min_dist = distance
+            self.clearCurrentMembers()
+            
+            ## Each input vector is presented to the map
+            for self.currIdx in range(len(self.traindata)-1): 
 
-                ## Step 4: All nodes within the neighbourhood of the BMU are changed, 
-                # you don't have to use distance relative learning.
-                upper_bound_d1 = min(self.n-1, min_d1 + int(radius))
-                lower_bound_d1 = max(0, min_d1 - int(radius))
-                upper_bound_d2 = min(self.n - 1, min_d2 + int(radius))
-                lower_bound_d2 = max(0, min_d2 - int(radius))
+                ## For each vector its Best Matching Unit is found  
+                self.findBMU()           
 
-                for dim_1 in range(lower_bound_d1, upper_bound_d1):
-                    for dim_2 in range(lower_bound_d2, upper_bound_d2):
-                        cluster = self.clusters[dim_1][dim_2]
-                        new_prot = []
-                        for vect_dim in range(self.dim):
-                            new_prot.append(float((1-learning_rate) * cluster.prototype[vect_dim] 
-                            + learning_rate * self.traindata[idx][vect_dim]))
-                        self.clusters[dim_1][dim_2].prototype = new_prot
-                self.clusters[min_d1][min_d2].current_members.add(idx)
+                 ## Calculate neighbourhood of BMU                                 
+                self.calculateNeighbourhood()    
 
+                ## All clusters within the neighbourhood of the BMU are changed            
+                self.updateClusterPrototypes()                
+
+                ## Add input vector to its BMU
+                self.clusters[self.min_d1][self.min_d2].current_members.add(self.currIdx)
+            
     def test(self):
-        ## iterate along all clients
-        hits = 0
-        requests = 0
-        prefetch = 0
-
-        ## for each client find the cluster of which it is a member
         for client in range(len(self.traindata)):
-            for dim_1 in range(self.n):
-                for dim_2 in range(self.n):
-                    if (client in self.clusters[dim_1][dim_2].current_members):
-                        owner = self.clusters[dim_1][dim_2]
+            self.findOwner(client) 
+            self.calculatePerformance(client)
 
-            ## get the actual testData (the vector) of this client
-            test = self.testdata[client]
-
-            ## iterate along all dimensions
-            for vect_dim in range(self.dim):
-                if(owner.prototype[vect_dim] > self.prefetch_threshold and 
-                    test[vect_dim] > self.prefetch_threshold):
-                    hits += 1         ## count number of hits
-                if(test[vect_dim] > self.prefetch_threshold):
-                    requests += 1     ## count number of requests
-                if(owner.prototype[vect_dim] > self.prefetch_threshold):
-                    prefetch += 1     ## count prefetched htmls
-
-        ## set the global variables hitrate and accuracy to their appropriate value
-        self.accuracy = hits/prefetch
-        self.hitrate = hits/requests
+        ## Set the global variables hitrate and accuracy to their appropriate value
+        self.accuracy = self.hits/self.prefetch
+        self.hitrate = self.hits/self.requests
 
     def print_test(self):
         print("Prefetch threshold =", self.prefetch_threshold)
